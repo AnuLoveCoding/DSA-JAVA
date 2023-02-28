d0131cda95d48872930b9c25ee618b088fdfe3cb
anuLoveCoading <anusrivas07@gmail.com>
2023-02-28 23:32:28 +0530

    git commit -m "DSA-JAVA"

--- src/DSA_ARRAY.java
+++ src/DSA_ARRAY.java
@@ -1,6 +1,34 @@
 import java.util.*;
 
 public class DSA_ARRAY {
+
+
+    public static void add(int [] arr){
+       int sum = 0;
+
+        for (int j : arr) {
+            sum += j;
+        }
+        System.out.println("The arr sum is : " + sum);
+    }
+
+    public static void max_arr(int [] arr){
+        int max = arr[0];
+        int min = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] > max){
+                max = arr[i];
+            }
+
+            if(arr[i] < min){
+                min = arr[i];
+            }
+
+        }
+        System.out.println("this is max arr is : "+ max);
+        System.out.println("this is min arr is : "+min);
+    }
     public static void main(String[] args){
         Scanner input = new Scanner (System.in);
 
@@ -14,9 +42,11 @@ public class DSA_ARRAY {
             arr[i] = input.nextInt();
         }
 
-
-        for (int j : arr) {
-            System.out.print(j+ " ");
+        for (int i = arr.length - 1; i >= 0 ; i--) {
+            System.out.print(arr[i]+" ");
         }
+
+//        add(arr);
+//       max_arr(arr);
     }
 }
