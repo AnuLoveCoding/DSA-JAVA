fa7c5df05a762f60689f4356c3ef61393a392275
anuLoveCoading <anusrivas07@gmail.com>
2023-03-01 01:16:27 +0530

    git commit -m "DSA-JAVA"

--- src/DSA_ARRAY.java
+++ src/DSA_ARRAY.java
@@ -29,18 +29,35 @@ public class DSA_ARRAY {
         System.out.println("this is max arr is : "+ max);
         System.out.println("this is min arr is : "+min);
     }
+
+    public static void transpose(int [][] arr1){
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = arr1[i][j];
+                arr1[i][j] = arr1[j][i];
+                arr1[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[0].length; j++) {
+                System.out.print(arr1[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args){
         Scanner input = new Scanner (System.in);
 
         System.out.println("Enter your arr size");
         int n = input.nextInt();
 
-        int [] arr = new int[n];
-
-        System.out.println("Enter your arr value ");
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = input.nextInt();
-        }
+//        int [] arr = new int[n];
+//
+//        System.out.println("Enter your arr value ");
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = input.nextInt();
+//        }
 
 
 
@@ -63,16 +80,52 @@ public class DSA_ARRAY {
 //        }
 
 //        print All the subarray;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i; j < arr.length; j++) {
-                for (int k = i; k <= j; k++) {
-                    System.out.print(arr[k]+ " ");
-                }
-                System.out.println();
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = i; j < arr.length; j++) {
+//                for (int k = i; k <= j; k++) {
+//                    System.out.print(arr[k]+ " ");
+//                }
+//                System.out.println();
+//            }
+//        }
+
+        int [][] arr1 = new int [n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr1[i][j] = input.nextInt();
             }
         }
 
 
+//        for (int i = 0; i < arr1.length; i++) {
+//            for (int j = 0; j < arr1[0].length; j++) {
+//                System.out.print(arr1[i][j]+ " ");
+//            }
+//            System.out.println();
+//        }
+
+//        Matrix Transpose;
+//        Approach 1 this is Brute-Force solution.
+//        int [][] arr2 = new int[n][n];
+//
+//        for (int j = 0; j < arr1[0].length; j++) {
+//            for (int i = 0; i < arr1.length; i++) {
+//                arr2[i][j] = arr1[i][j];
+//            }
+//        }
+
+//      Matrix Transpose;
+//      Approach 2 this is optimized solution.(### Here no need to take extra space for store the value);
+
+//        for (int i = 0; i < arr1.length; i++) {
+//            for (int j = 0; j < i; j++) {
+//                int temp = arr1[i][j];
+//                arr1[i][j] = arr1[j][i];
+//                arr1[j][i] = temp;
+//            }
+//        }
+
+        transpose(arr1);
 
 //        System.out.println(arr);
 
