7f5af1b92dedb71a268b8711287edd7b87780e39
anuLoveCoading <anusrivas07@gmail.com>
2023-02-28 23:08:04 +0530

    DSA-JAVA

--- src/DSA_ARRAY.java
+++ src/DSA_ARRAY.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class DSA_ARRAY {
+import java.util.*;
+
+public class DSA_ARRAY {
+    public static void main(String[] args){
+        Scanner input = new Scanner (System.in);
+
+        System.out.println("Enter your arr size");
+        int n = input.nextInt();
+
+        int [] arr = new int[n];
+
+        System.out.println("Enter your arr value ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = input.nextInt();
+        }
+
+        for (int j : arr) {
+            System.out.print(j+ " ");
+        }
+    }
 }
