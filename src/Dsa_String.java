119e539d80827525fc1c4e37a533470c563bb9dd
anuLoveCoading <anusrivas07@gmail.com>
2023-03-02 14:10:27 +0530

    git commit -m "DSA-JAVA"

--- src/Dsa_String.java
+++ src/Dsa_String.java
@@ -4,8 +4,10 @@ public class Dsa_String {
     public static void main(String [] args){
         Scanner scn =  new Scanner(System.in);
 
-        System.out.println("This is my favorite name, did you know?  " + scn.nextLine());
 
+        String name  = scn.nextLine();
+
+        System.out.printf("This is my first name, did  you  know? " + name);
 
     }
 }
