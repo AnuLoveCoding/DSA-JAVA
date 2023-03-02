b02fb64a72c8c634847d3b2db80983e451c920fb
anuLoveCoading <anusrivas07@gmail.com>
2023-03-02 15:06:40 +0530

    git commit -m "DSA-JAVA"

---
+++ src/Dsa_HashMap.java
@@ -0,0 +1,5 @@
+public class Dsa_HashMap {
+    public static void main(String[] args) {
+        System.out.printf("Hello");
+    }
+}
--- src/Dsa_String.java
+++ src/Dsa_String.java
@@ -2,12 +2,9 @@ import java.util.*;
 
 public class Dsa_String {
     public static void main(String [] args){
-        Scanner scn =  new Scanner(System.in);
+        Scanner input =  new Scanner(System.in);
 
 
-        String name  = scn.nextLine();
-
-        System.out.printf("This is my first name, did  you  know? " + name);
 
     }
 }
