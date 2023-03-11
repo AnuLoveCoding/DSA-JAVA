ec99217cbcef8e80461b4b83a415db41f329f468
anuLoveCoading <anusrivas07@gmail.com>
2023-03-11 13:16:55 +0530

    git commit -m "DSA-JAVA"

--- src/tower_of_hanoi.java
+++ src/tower_of_hanoi.java
@@ -1,5 +1,39 @@
+import java.util.*;
+
 public class tower_of_hanoi {
+    static ArrayList<ArrayList<Integer>> ans;
+
+    public static void toh(int A , int from, int to, int aux){
+        if(A == 0){
+            return;
+        }
+
+        toh(A - 1 , from, aux, to);
+
+        ArrayList<Integer>ls = new ArrayList<>();
+
+        ls.add(A);
+        ls.add(from);
+        ls.add(to);
+
+        ans.add(ls);
+
+        toh(A - 1, aux, to , from);
+
+    }
+    public static void tower_of_hanoi1(int A){
+        ans = new ArrayList<ArrayList<Integer>>();
+
+        toh(A , 1 , 3, 2);
+
+        System.out.println(ans);
+    }
     public static void main(String[] args) {
+
+        int A = 3;
+
+        tower_of_hanoi1(A);
+
 
     }
 }
